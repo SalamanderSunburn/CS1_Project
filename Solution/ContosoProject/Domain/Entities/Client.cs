da15a8b3ff1fc3564d8cb6ec8b89f89e416c8405
imnikoo <efruev@gmail.com>
2015-10-14 15:33:01 +0300

    ClientFix

--- Solution/ContosoProject/Domain/Entities/Client.cs
+++ Solution/ContosoProject/Domain/Entities/Client.cs
@@ -8,13 +8,8 @@ namespace Domain.Entities
         public Location ClientLocation { get; set; }
         public Person Person { get; set; }
         private ICollection<string> _telephones;
-<<<<<<< HEAD
         private ICollection<Comment> _comments;
         public Client(ICollection<string> telephones, ICollection<Comment> comments)
-=======
-
-        public Client(ICollection<string> telephones)
->>>>>>> parent of 3a07962... Revert "ClientForm design and 75% impl."
         {
             _telephones = telephones;
             _comments = comments;
@@ -27,12 +22,9 @@ namespace Domain.Entities
             }
         }
 
-<<<<<<< HEAD
         public IReadOnlyCollection<Comment> Comments
         {
             get { return (IReadOnlyCollection<Comment>)_comments; }
         }
-=======
->>>>>>> parent of 3a07962... Revert "ClientForm design and 75% impl."
     }
 }
